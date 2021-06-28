aab2aa1aa1a02d99035e1c7afebff3658e6fbf00
Amit Genish <amitgk159753@gmail.com>
2021-06-29 01:56:45 +0300

    Creating Cleaner class

---
+++ MaternityWard/Cleaner.cs
@@ -0,0 +1,7 @@
+﻿namespace MaternityWard
+{
+    class Cleaner : AdministrativeWorker
+    {
+        public Cleaner(WorkerRank[] ranks, int? riskPercentage = null) : base(ranks: AddRankFirst(ranks, WorkerRank.minor), riskPercentage) { }
+    }
+}
