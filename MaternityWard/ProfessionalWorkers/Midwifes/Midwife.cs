19d1823d223f1102203892bd4a68e788245004fe
Amit Genish <amitgk159753@gmail.com>
2021-06-29 23:55:47 +0300

    Created Midwife class

---
+++ MaternityWard/ProfessionalWorkers/Midwifes/Midwife.cs
@@ -0,0 +1,7 @@
+﻿namespace MaternityWard.ProfessionalWorkers.Midwife
+{
+    class Midwife : ProfessionalWorker
+    {
+        public Midwife(WorkerRank[] ranks) : base(ranks: AddRankFirst(ranks, WorkerRank.senior)) { }
+    }
+}
