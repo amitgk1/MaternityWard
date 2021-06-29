66c5dd0ba2163d65d7cbae6a458b0118ab80fe9f
Amit Genish <amitgk159753@gmail.com>
2021-06-30 02:21:37 +0300

    ranks classes

---
+++ MaternityWard/Ranks/HourlyPaidRanks/DecisionMakerRank.cs
@@ -0,0 +1,15 @@
+﻿namespace MaternityWard
+{
+    class DecisionMakerRank : IHourlyPaidRank
+    {
+        public double calculateSalary(double workHours)
+        {
+            double salary = workHours * Config.HOURLY_WAGE * 1.5; // 50% increase
+            if (workHours > 50)
+            {
+                salary += 200 * Config.HOURLY_WAGE;
+            }
+            return salary;
+        }
+    }
+}
